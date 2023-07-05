4ebf48231ce36b40d6cb3080ae9d6c6ff57f6dc4
ArthurMaslov <131531263+ArthurMaslov@users.noreply.github.com>
2023-07-05 19:44:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,11 +10,32 @@ hamburger.onclick = function () {
 
 
 
-for (let i = 0; i < 4; i++) {
-    navLinksA[i].onclick = function () {
+for (let i = 0; i < 4; i++) {//цикл ,который срабатывает 4 раза
+    navLinksA[i].onclick = function () {// если нажать на любую из ссылок запускается функция
         hamburger.classList.toggle('hamburger_active');
         navLinks.classList.toggle('nav__links_active');
     }
 }
 
 
+
+
+
+
+
+
+
+
+
+
+let form = document.querySelector('form');
+let iframe = document.querySelector('iframe');
+let sendWrapper = document.querySelector('.send-wrapper');
+
+iframe.onload = function () {//запуск функции при загрузке в iframe
+    sendWrapper.classList.add('send-wrapper_active');
+    setTimeout(() => {//функция ,которая срабатывает через 3 секунду
+        form.reset();//сброс формы
+        sendWrapper.classList.remove('send-wrapper_active');
+    }, 3000);
+}
